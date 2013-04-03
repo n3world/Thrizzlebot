1a3d9d48d809edc492669819c28b7dd1b781f250
Matt Farmer <work@mattfarmer.net>
2013-04-03 09:33:53 -0700

    Add '!seen' function

--- bot.js
+++ bot.js
@@ -51,4 +51,55 @@
   bot.summon = function (packet) {
     bot.say(packet.nick, "If this was working I would summon: " + packet.args[0]);
   };
+
+  //-- Seen module
+  //(function(bot) {
+  var seen = {};
+
+  /**
+   * Record when we observe an action by a user.
+   */
+  function observer(action, channel, who) {
+    if (who !== config.botName) {
+      if (!seen[channel]) {
+        seen[channel] = {};
+      }
+      seen[channel][who] = {
+        action: action,
+        date: new Date()
+      };
+    }
+  }
+
+  /**
+   * Answer '!seen' requests.
+   */
+  function responder(who, channel, text, packet) {
+    var params = text.split(/\s/),
+        subject = params[1];
+    if (seen[channel] && seen[channel][subject]) {
+      var last = seen[channel][subject];
+      bot.say(channel, who + ": " + subject + " was last seen " + last.action + " at " + last.date);
+    } else {
+      bot.say(channel, who + ": Seen who?");
+    }
+  }
+
+  /**
+   * Initialize listeners.
+   */
+  function init() {
+    bot.addListener("join", function (channel, who) {
+      return observer("joining: " + channel, channel, who);
+    });
+    bot.addListener("message#", function (who, channel, text, packet) {
+      if (0 === text.indexOf('!seen')) {
+        responder(who, channel, text, packet);
+      }
+      return observer("saying: '" + text + "'", channel, who);
+    });
+  }
+
+  init();
+  //})(bot);
 })();
