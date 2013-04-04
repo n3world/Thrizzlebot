bc5135f08a2bef1bfab0726a5f98023647a1436c
Matt Farmer <work@mattfarmer.net>
2013-04-03 23:02:14 -0700

    Added .editorconfig and .jshint files and fixed bot.js to validate

--- bot.js
+++ bot.js
@@ -1,50 +1,54 @@
-var irc = require("irc");
+(function () {
+  "use strict";
 
-var config = {
-  channels: ["#npnd-dev"],
-  server: "irc.freenode.net",
-  botName: "ThrizzleBot"
-};
+  var irc = require("irc");
 
-// Create the bot name
-var bot = new irc.Client(config.server, config.botName, {
-  channels: config.channels
-});
+  var config = {
+    channels: ["#npnd-dev"],
+    server: "irc.freenode.net",
+    botName: "ThrizzleBot"
+  };
 
-// Listen for joins
-bot.addListener("join", function(channel, who) {
-  if(who !== config.botName) {
-    bot.say(channel, who + " WHATUUUP");
-  }
-});
+  // Create the bot name
+  var bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+  });
 
-/**
- * nick = string. nick of person pm-ing
- * text = string, content of pm
- * message = object, detailed info about user and messsage
-**/
-bot.addListener("pm", function(nick, text, message) {
-  var packet = {};
-  packet.nick = nick;
-  packet.args = text.split(/\s/g);
+  // Listen for joins
+  bot.addListener("join", function (channel, who) {
+    if (who !== config.botName) {
+      bot.say(channel, who + " WHATUUUP");
+    }
+  });
 
-  var command = this[packet.args[0]];
-  if ('function' !== typeof command) { 
-    bot.say(nick, "I don't know what you're talking about");
-  } else {
-    packet.args.splice(0,1);
-    return command(packet);
-  }
-});
+  /**
+   * nick = string. nick of person pm-ing
+   * text = string, content of pm
+   * message = object, detailed info about user and messsage
+   **/
+  bot.addListener("pm", function (nick, text, message) {
+    var packet = {};
+    packet.nick = nick;
+    packet.args = text.split(/\s/g);
 
-// Tweets whatever you send this to the @NPND Account
-// OP status in #npnd required
-bot.tweet = function(packet) {
-  bot.say(packet.nick, "If this was working I would tweet: " + packet.args[0]);
-}
+    var command = this[packet.args[0]];
+    if ('function' !== typeof command) {
+      bot.say(nick, "I don't know what you're talking about");
+    } else {
+      packet.args.splice(0, 1);
+      return command(packet);
+    }
+  });
 
-// Summons a registered user to the channel for discussion
-// Requires contact method to be registered with ThrizzleBot
-bot.summon = function(packet) {
-  bot.say(packet.nick, "If this was working I would summon: " + packet.args[0]);
-}
+  // Tweets whatever you send this to the @NPND Account
+  // OP status in #npnd required
+  bot.tweet = function (packet) {
+    bot.say(packet.nick, "If this was working I would tweet: " + packet.args[0]);
+  };
+
+  // Summons a registered user to the channel for discussion
+  // Requires contact method to be registered with ThrizzleBot
+  bot.summon = function (packet) {
+    bot.say(packet.nick, "If this was working I would summon: " + packet.args[0]);
+  };
+})();
