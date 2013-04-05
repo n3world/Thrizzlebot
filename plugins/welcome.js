d48cf504b413048d4698f2066e073ad39650fdbb
Mark Henderson <henderson.mark@gmail.com>
2013-04-04 20:39:55 -0400

    Adding welcome module

---
+++ plugins/welcome.js
@@ -0,0 +1,30 @@
+exports.init = (function() {
+  "use strict";
+  var _bot,
+      config = require("../config"),
+      util = require('util');
+
+  /**
+   * Listens for joins and sends a welcome message
+  */
+  function listenForJoin(channel, who) {
+    var pattern,
+        salutation;
+
+    if (who !== config.botName) {
+      pattern = config.plugin.welcome.salutation;
+      salutation = util.format(pattern, who);
+      _bot.say(channel, salutation);
+    }
+  }
+
+  /**
+   * Initialize listeners.
+   */
+  function init(bot) {
+    _bot = bot;
+    _bot.addListener("join", listenForJoin);
+  }
+
+  return init;
+})();
