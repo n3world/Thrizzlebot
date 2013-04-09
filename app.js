26aa28a7916109cbd543d406abd7b028e72d4809
Mark Henderson <henderson.mark@gmail.com>
2013-04-08 23:07:05 -0400

    Running a standalone web server to maintain a persistent connection

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-bot = require("./bot");
+var bot = require("./bot"),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Thrizzlebot is running in #npnd\n');
+}).listen(1337, '127.0.0.1');
