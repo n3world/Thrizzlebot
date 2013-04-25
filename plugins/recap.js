38372fe262e67b2dd5a3c114f8b42fae445cdc9b
Nate Clark <nate@neworld.us>
2013-04-25 18:52:01 -0400

    Add tracking of parts to recap
    Now recap only prints what happened since the nick left the room.

--- plugins/recap.js
+++ plugins/recap.js
@@ -12,6 +12,15 @@ function Recap(bot) {
 
   this._bot = bot;
   this._msgs = {};
+  this._parts = {};
+}
+
+Recap.prototype._lastSeen = function(channel, nick) {
+  var lastSeen = 0;
+  if (this._parts[channel] !== undefined && this._parts[channel][nick] !== undefined) {
+    lastSeen = this._parts[channel][nick];
+  }
+  return lastSeen;
 }
 
 // respondToCommand implementation for recap
@@ -19,16 +28,46 @@ Recap.prototype.respondToCommand = function(nick, channel, isPm, args) {
   if (this._msgs[channel] !== undefined) {
     var msgs = this._msgs[channel];
     var numToReturn = this.defaultReturn;
+    var lastSeen = this._lastSeen(channel, nick);
 
     if (args.length > 0) {
       numToReturn = parseInt(args[0]);
     }
 
     var firstMsg = Math.max(msgs.length - numToReturn, 0);
+    // Collect all messages which occurred after the nick last left
+    var recaps = [];
     for (var i = firstMsg; i < msgs.length; ++i) {
-       this._bot.say(nick, msgs[i].nick + ": " + msgs[i].msg);
+       if (lastSeen <= msgs[i].time) {
+         recaps.push(msgs[i].nick + ": " + msgs[i].msg);
+       }
+    }
+
+    // send response
+    if (recaps.length > 0) {
+      this._bot.say(nick, recaps.join("\n"));
+    } else {
+      this._bot.say(nick, "Nothing has happened");
     }
   }
+}
+
+Recap.prototype.recordMessage = function(who, channel, text) {
+  if (this._msgs[channel] === undefined) {
+    this._msgs[channel] = [];
+  }
+
+  var len = this._msgs[channel].push({nick: who, msg: text, time: new Date().getTime()});
+  if (len > this.maxSize) {
+    this._msgs[channel].splice(0, len - this.maxSize);
+  }
+}
+
+Recap.prototype.recordPart = function(channel, nick) {
+  if (this._parts[channel] === undefined) {
+    this._parts[channel] = {};
+  }
+  this._parts[channel][nick] = new Date().getTime();
 }
 
 // Function for initializing a new recap instance
@@ -37,14 +76,11 @@ exports.init = (function() {
   var recap;
 
   function listenForMessage(who, channel, text, packet) {
-    if (recap._msgs[channel] === undefined) {
-      recap._msgs[channel] = [];
-    }
+    recap.recordMessage(who, channel, text);
+  }
 
-    var len = recap._msgs[channel].push({nick: who, msg: text});
-    if (len > recap.maxSize) {
-      recap._msgs[channel].splice(0, len - recap.maxSize);
-    }
+  function listenForPart(channel, nick, reason, message) {
+    recap.recordPart(channel, nick);
   }
 
   // Function that is called to run initialization
@@ -52,6 +88,7 @@ exports.init = (function() {
     recap = new Recap(bot);
     baseInit(bot, recap, config, true);
     bot.addListener("message#", listenForMessage);
+    bot.addListener("part", listenForPart);
   }
 })()
 
