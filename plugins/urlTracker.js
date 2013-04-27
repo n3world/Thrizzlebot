d3b0f203f60834e7b240105329618599702eaacd
Nate Clark <nate@neworld.us>
2013-04-26 21:08:13 -0400

    Add details to urlTracker
    This adds the ability to pull in details about a given url. When a url
    is seen the bot will try perform a get on the url and parse out the
    title ad description of the returned resource

--- plugins/urlTracker.js
+++ plugins/urlTracker.js
@@ -10,8 +10,13 @@ function UrlTracker(bot) {
   this.maxArgs = 2;
 
   this._bot = bot;
-  this._urlRegex = new RegExp("https?://");
   this._urls = {};
+  this._urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/g;
+
+  this._url = require('url');
+  this._http = require('http');
+  this._https = require('https');
+  this._htmlparser = require('htmlparser2');
 }
 
 // respondToCommand implementation for urlTracker
@@ -43,26 +48,123 @@ UrlTracker.prototype.respondToCommand = function(nick, channel, isPm, args) {
 
      for (var i = 0, returned = 0; i < urls.length && returned < numToReturn; ++i) {
        if (nickFilter === undefined || urls[i].nick == nickFilter) {
-         this._bot.say(target, urls[i].msg);
+         this._bot.say(target, urls[i].toString());
          ++returned;
       }
     }
   }
 }
 
+// Track urls that are in the given text
 UrlTracker.prototype.trackMessage = function(nick, channel, text) {
-  if (this._urlRegex.test(text)) {
+  var match;
+  while (match = this._urlRegex.exec(text)) {
     if (this._urls[channel] === undefined) {
       this._urls[channel] = [];
     }
 
-    var len = this._urls[channel].unshift({nick: nick, msg: text});
+    var urlInfo = new UrlInfo(nick, match[0]);
+    var len = this._urls[channel].unshift(urlInfo);
     if (len > this.maxSize) {
       this._urls[channel].splice(this.maxSize, len - this.maxSize);
     }
+    this._getDetails(urlInfo);
   }
 }
 
+// Request the document described by urlInfo and update urlInfo with details
+UrlTracker.prototype._getDetails = function(urlInfo) {
+  var body;
+  var url = this._url.parse(urlInfo.url);
+  var client,
+      protocol = url.protocol;
+  var _this = this;
+
+  if (!protocol) {
+    // unknown protocol lets try http
+    client = this._http;
+    url = "http://" + urlInfo.url;
+  } else if (protocol == "http:") {
+    client = this._http;
+  } else if(protocol == "https") {
+    client = this._https;
+  } else {
+    return;
+  }
+
+  client.get(url, function(res) {
+    res.on('data', function (chunk) {
+      _this._parseDetails(chunk, urlInfo);
+    });
+  });
+}
+
+// Parse extra details out of the html body
+UrlTracker.prototype._parseDetails = function(page, urlInfo) {
+  var textDestination;
+  var extraHead = false;
+
+  var parser = new this._htmlparser.Parser({
+    onopentag: function(name, attribs) {
+      if (name == "title") {
+        textDestination = name;
+	extraHead = true;
+      } else if (name == "meta" && attribs.name == "description") {
+        urlInfo.addExtra(attribs.name, attribs.content);
+
+      }
+    },
+
+    ontext: function(text) {
+      if (textDestination !== undefined) {
+        urlInfo.addExtra(textDestination, text, extraHead);
+      }
+    },
+
+    onclosetag: function(tagname) {
+      textDestination = undefined;
+      extraHead = false;
+    }
+  });
+
+  parser.write(page);
+  parser.end();
+}
+
+// Helper object to contain url information
+function UrlInfo(nick, url) {
+  this.nick = nick;
+  this.url = url;
+  this._extraInfo = [];
+}
+
+UrlInfo.prototype.toString = function() {
+  var string = this.nick + ": "  + this.url;
+
+  for (var i  in this._extraInfo) {
+    if (this[this._extraInfo[i]] !== undefined) {
+      string += "\n    " + this[this._extraInfo[i]];
+    }
+  }
+  return string;
+}
+
+/*
+ * Add in extra url information
+ * @param name name of information
+ * @value information to add
+ * @head whether or not insert this at the head of the list of information
+ */ 
+UrlInfo.prototype.addExtra = function(name, value, head) {
+  this[name] = unescape(value);
+  if (head) {
+    this._extraInfo.unshift(name);
+  } else {
+    this._extraInfo.push(name);
+  }
+}
+
+// Create the init function for a urlTracker plugin
 exports.init = (function() {
   "use strict";
   var urlTracker;
